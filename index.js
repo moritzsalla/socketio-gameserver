2057b2cd20d8e14485f3092a27e3a26ff10138d5
Moritz Salla <moritz.salla@hotmail.de>
2020-05-30 15:14:45 +0200

    changed console log to dynamic port

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const FALLBACK_PORT = '5039'; // Heroku uses port 5039 as standard
+const FALLBACK_PORT = '5039'; // Heroku uses random port number as standard
 var io = require('socket.io')(process.env.PORT || FALLBACK_PORT);
 
 // CUSTOM CLASSES
 var Player = require('./classes/player.js');
 
-console.log(`Server has started on port ${FALLBACK_PORT}`);
+console.log(`Server has started on port ${process.env.PORT}`);
 
 var players = [];
 var sockets = [];
